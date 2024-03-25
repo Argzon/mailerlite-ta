821fcddde0cf0649c507aa278357575a2cb6bcd6
Argzon <argzon01@gmail.com>
2024-03-25 04:55:23 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    colors: {
+      primary: "#09C269",
+      lightGreen: "#CEF3E1",
+      heading: "#000000",
+      text: "#000000",
+      background: "#FAFBFC",
+      border: "#E3E3E3",
+    },
+  },
+  plugins: [],
+};
